2f5330f0ccb902f8f8efde24df954e0bc6190aff
庞树博 <shubo.pang@hc-system.com>
2023-04-17 08:53:42 +0800

    jenkins 增加静态库编译

--- jenkins_rzg2l.sh
+++ jenkins_rzg2l.sh
@@ -20,8 +20,25 @@ function buildRZG2L(){
     cp ${output_path}/* ${jenkins_output_path} -rf
 }
 
+function buildstaticlibRZG2L(){
+    SDK_path=/opt/poky
+    #SDK_path=/home/ubuntu/poky
+    project_path=.
+    unset LD_LIBRARY_PATH
+    source ${SDK_path}/3.1.5/environment-setup-aarch64-poky-linux
+    cd ${project_path} && rm ${project_path}/output/* -rf &&
+    rm Makefile -rf &&
+    ${SDK_path}/3.1.5/sysroots/x86_64-pokysdk-linux/usr/bin/qt5/qmake  ${project_path}/modbus.pro CONFIG += staticlib -spec linux-oe-g++ && 
+    /usr/bin/make clean -j8 && 
+    /usr/bin/make qmake_all &&
+    make -j8  && 
+    output_path=${project_path}/output/ && 
+    cp ${output_path}/* ${jenkins_output_path} -rf
+}
+
+
 # 设置需要构建的参数  可选参数
 
 buildRZG2L
-
+buildstaticlibRZG2L
 
